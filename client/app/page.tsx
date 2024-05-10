8515379d3cf8a062b49d7528675bd760a83d3b64
Dev25-os <bikeshgupta50041@gmail.com>
2024-05-11 00:05:22 +0530

    added custom table

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,13 @@
 import Header from "@/components/Tickets/Header";
+import TicketsDataTable from "@/components/Tickets/TicketsDataTable";
 
 const Home = () => {
   return (
     <div className="p-4 ">
       <Header />
+      <div className="table py-4 w-full">
+        <TicketsDataTable />
+      </div>
     </div>
   );
 };
---
+++ client/components/Tickets/TicketsDataTable.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { CustomTable } from "@/components/CustomTable";
+import { ColumnDef } from "@tanstack/react-table";
+
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+const TicketsDataTable = () => {
+  const columns: ColumnDef<Payment>[] = [
+    {
+      accessorKey: "status",
+      header: "Status",
+    },
+    {
+      accessorKey: "email",
+      header: "Email",
+    },
+    {
+      accessorKey: "amount",
+      header: "Amount",
+    },
+  ];
+  const payments: Payment[] = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "489e1d42",
+      amount: 125,
+      status: "processing",
+      email: "example@gmail.com",
+    },
+  ];
+  return (
+    <div>
+      <CustomTable data={payments} columns={columns} />
+    </div>
+  );
+};
+
+export default TicketsDataTable;
